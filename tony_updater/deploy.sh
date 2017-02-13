1e6b1dcf4f99b4715b02eee0d3226397f94e1052
fakuivan <fakuivan@gmail.com>
2017-02-13 01:00:23 -0300

    Set email and name to git before commiting

--- tony_updater/deploy.sh
+++ tony_updater/deploy.sh
@@ -6,6 +6,8 @@ PROJECT_VERSION=$2
 UPDATER_REPO=$3
 UPDATER_USER=$4
 OAUTH_TOKEN=$5
+USER_NAME=$6
+USER_EMAIL=$7
 mkdir "$UPDATER_PATH"
 git clone "https://github.com/$UPDATER_USER/$UPDATER_REPO.git" "$UPDATER_PATH"
 rm -rf "$UPDATER_PATH/$PROJECT_NAME"
@@ -13,5 +15,7 @@ mkdir "$UPDATER_PATH/$PROJECT_NAME"
 cp -r "$PACKAGE_FILES/." "$UPDATER_PATH/$PROJECT_NAME"
 cd "$UPDATER_PATH"
 git add "./$PROJECT_NAME/" -A
+git config --global user.name $USER_NAME
+git config --global user.email $USER_EMAIL
 git commit -m "Update '$PROJECT_NAME' to version $PROJECT_VERSION"
 git push "https://$OAUTH_TOKEN@github.com/$UPDATER_USER/$UPDATER_REPO.git"
